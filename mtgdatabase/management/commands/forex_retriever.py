ae14dc70cf243ec0407d3fa1f54cf53d0728df07
Campbell Sinclair <watsondr@users.noreply.github.com>
2021-08-13 23:12:47 +1000

    fix forex grabber
    change api vendor for forex pair prices

--- mtgdatabase/management/commands/forex_retriever.py
+++ mtgdatabase/management/commands/forex_retriever.py
@@ -36,18 +36,34 @@ class Command(BaseCommand):
         ===============================================================
         """
         
-        fxPair = Forex.objects.filter(pair__exact="USD/AUD").first()
+        with open("mtgdatabase\static\mtgdatabase\json\credentials.json", "r") as read_file:
+            credentials_data = json.load(read_file)
+            
+        KEY = credentials_data["currency-converter"]["x-rapidapi-key"]
+        HOST = credentials_data["currency-converter"]["x-rapidapi-host"] 
         
-        url = "https://api.exchangeratesapi.io/latest?base=USD&symbols=USD,AUD"
+        url = "https://currency-converter5.p.rapidapi.com/currency/convert"
+        
+        querystring = {"format":"json","from":"USD","to":"AUD","amount":"1"}
+        
+        headers = {
+            'x-rapidapi-key': KEY,
+            'x-rapidapi-host': HOST
+            }
         
         try:
-            req = requests.get(url)
+            req = requests.get(url, headers=headers, params=querystring)
             req.raise_for_status()
             payload = json.loads(req.text)
-            price = payload["rates"]["AUD"]
+            price = payload["rates"]["AUD"]["rate"]
         except requests.exceptions.HTTPError as e:
             print(e.response.text)
-            price = 1
+            """
+            No longer set price here as it impedes debugging,
+            if vendor service has a policy change.
+            """
+        
+        fxPair = Forex.objects.filter(pair__exact="USD/AUD").first()
         
         update_latest_rate(price, fxPair)
         
